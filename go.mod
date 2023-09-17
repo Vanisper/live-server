b5306a972753a938ad402a52f7ff64b533f8fd16
Vanisper <273266469@qq.com>
2023-09-17 17:08:31 +0800

    实现/assets-handler-x/yyyy本地资源访问

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25
 	github.com/stretchr/testify v1.8.1
-	github.com/wailsapp/wails/v2 v2.6.0
+	github.com/wailsapp/wails/v2 v2.5.1
 )
 
 require (
@@ -27,7 +27,6 @@ require (
 	github.com/quic-go/quic-go v0.38.1 // indirect
 	github.com/refraction-networking/utls v1.5.3 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/wailsapp/go-webview2 v1.0.1 // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/tools v0.12.1-0.20230815132531-74c255bcf846 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"embed"
+	"live-server/pkg/utils"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -13,6 +16,53 @@ import (
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+type FileLoader struct {
+	http.Handler
+}
+
+func NewFileLoader() *FileLoader {
+	return &FileLoader{}
+}
+
+func (h *FileLoader) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	log.Println(req.URL.Path)
+	requestedFilename := strings.TrimPrefix(req.URL.Path, "/") // 获取文件名称 assets-handler-x/xxxx
+	log.Println(requestedFilename)
+	if requestedFilename == "" {
+		// 判断是否是读取index.html
+		requestedFilename = "index.html"
+	}
+	// 静态资源基础路径设置为程序运行位置
+	assetBasePath := utils.GetCurrPath() + "\\"
+	// 先判断是不是assets-handler-x/xxxx  获取其中的盘符
+	if strings.HasPrefix(requestedFilename, "assets-handler-") {
+		// 获取盘符
+		assetBasePath = requestedFilename[15:16] + "://"
+		// 获取文件名称
+		requestedFilename = requestedFilename[17:]
+	}
+	assetFullPath := assetBasePath + requestedFilename
+	log.Println(assetFullPath)
+	http.ServeFile(res, req, assetFullPath)
+	//fileData, err := os.ReadFile( assetFullPath)
+	///*
+	//	读取程序的运行路径下的对应资源文件
+	//	这里的资源前缀可以自定义，如果是本地资源的话就使用os.ReadFile的方式获取文件，
+	//	如果是网络资源的话，可以使用 http.ServeFile() 方法获取资源文件。
+	//	当然，也可以统一成http.ServeFile，就是在本地以某个文件夹起一个server即可
+	//	---
+	//	该方法是在 assets 中获取不到资源的情况下执行的，这种场景一般出现在：前端网页中有些网络资源可能失效了，
+	//	所以在assets出现404的情况下，就会执行handler方法，用于可能的补救措施，
+	//	或者说是前端有意为之的，变相地将本地资源“转发”到前端所处端口下
+	//*/
+	//if err != nil {
+	//	println(err.Error())
+	//	res.WriteHeader(http.StatusBadRequest)
+	//	res.Write([]byte(fmt.Sprintf("无法加载文件 %s", requestedFilename)))
+	//}
+	//res.Write(fileData)
+}
 
 //go:embed build/appicon.png
 var icon []byte
@@ -31,7 +81,8 @@ func main() {
 		Frameless:   true,
 		StartHidden: isDev,
 		AssetServer: &assetserver.Options{
-			Assets: assets,
+			Assets:  assets,
+			Handler: NewFileLoader(),
 		},
 		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 0},
 		OnStartup:        app.startup,
